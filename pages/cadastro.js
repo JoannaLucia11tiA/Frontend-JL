87470ed5fe68665b6044b550e560661c610d4c5a
Nickolas-Zilig-Oliveira <joannalucia25@gmail.com>
2025-11-11 22:56:31 -0300

    Parte 2 tarefa fim

---
+++ pages/cadastro.js
@@ -0,0 +1,38 @@
+const button = document.querySelector("button")
+button.onclick = (event) => {
+    event.preventDefault()
+    signUpFriends()
+}
+
+async function signUpFriends(){
+    const name = document.querySelector("#name").value
+    const email = document.querySelector("#email").value
+    const age = document.querySelector("#age").value
+    const nickname = document.querySelector("#nickname").value
+    const password = document.querySelector("#password").value
+
+    if(name === ""|| email === "" || age === ""|| nickname === ""||password === ""){
+        alert("PREENCHA TODAS AS INFORMAÇÕES, ANIMAL!")
+        return
+    }
+
+    const user = {
+        name,
+        email,
+        age,
+        nickname,
+        password
+    }
+    const response = await fetch("http://localhost:3333/cadastrar", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user })
+    }).then(response => response.json())
+
+    const {message} = response
+    alert(message)
+
+    window.location.href = "../index.html"
+}
